c229ccea9c96eaea90387c1cfd88148ccc775266
vagurtomejia <veronica.agurto.mejia@gmail.com>
2018-02-05 16:08:00 +0100

    Adds RecipesFinder model

---
+++ app/models/recipes-finder.rb
@@ -0,0 +1,13 @@
+class RecipesFinder
+
+  include HTTParty
+
+  base_uri 'http://food2fork.com/api'
+  default_params fields: "smallIcon,shortDescription", q: "search", key: ENV["FOOD2FORK_KEY"]
+  format :json
+
+  def self.for term
+    get("/search", query: { q: term})["recipes"]
+  end
+
+end
