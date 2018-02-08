9c97a9f1e3cad1f6e89b38e3968d9c47b213d5e1
vagurtomejia <veronica.agurto.mejia@gmail.com>
2018-02-08 14:51:19 +0100

    Changes query default params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @search_term = params[:search] || 'vegan' #defaults to vegan if no request param
+    @search_term = params[:search] || 'vegan gluten free' #defaults to vegan if no request param
     @recipes = Recipe.for(@search_term) #array of recipes found
   end
 end
